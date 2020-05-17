3d5718264afca4388b376c2bcb59dd180b9b9457
Heber Leonard <heber.leonard2@gmail.com>
2020-05-17 19:22:31 -0300

    Criar e Deletar repositorio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=> {
+    api.get('/repositories').then(res => {
+      setRepositories(res.data);
+    })
+  },[])
   async function handleAddRepository() {
-    // TODO
+   const response = await api.post('/repositories', {
+    title: `Novo repositório ${Date.now()}`,
+    url: "www.github.com.br",
+    techs: ["Nodejs", "ReactJs"]
+   });
+
+   const repository = response.data;
+   setRepositories([...repositories, repository]);
+   
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    await api.delete(`/repositories/${id}`)
+    
+    setRepositories(repositories.filter(item => item.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        
+        { repositories.map(repository => <li key={repository.id}>{repository.title} <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
-          </button>
-        </li>
+          </button></li> ) }
+        
+        
+
+         
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
